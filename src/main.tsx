cad27d6e7c12b29db1b6b67e697ce1c10f2a80bf
Anton <deragandi0@gmail.com>
2025-06-08 11:52:47 +0300

    changed route for AppRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Providers } from './app/providers/Providers';
-import { AppRouter } from './app/routing/appRouter';
+import { AppRouter } from './app/routing/AppRouter';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
